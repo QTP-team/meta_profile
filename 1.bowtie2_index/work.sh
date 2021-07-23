7d46892847b8b23195f1e8c3e6b9f2ab542b9d9b
TianLiu <tianliu@genomics.cn>
2021-07-23 15:17:41 +0800

    init
    Reorganized almost the entire pipeline

--- 1.bowtie2_index/work.sh
+++
@@ -1,1 +0,0 @@
-bowtie2-build --threads 4 ../0.data/merge_sp3.fna merge_sp3_bowtie2_index
--- rules/profile_bowtie2.py
+++ rules/profile_bowtie2.py
@@ -2,30 +2,31 @@ import argparse
 import pysam
 import re
 import subprocess
+import os
 
 parser = argparse.ArgumentParser(
     prog = "profile_bowtie2.py",
-    usage = "python profile.py -x bowtie2_index -r1 {rmhost_reads1} -r2 {rmhost_reads2} -contig {genome_contig_length_file} -ID {genome_ID_file} -t {threads} -i {identity} -n {reads_num} -o {output_prefix}")
+    usage = "python rules/profile_bowtie2.py -x 1.index/merge_sp3_bowtie2_index -r1 0.data/test1_data_R1.fastq -r2 0.data/test1_data_R2.fastq -contig 1.index/genome_contig_length.txt -t 4 -i 0.95 -n 100 -o test1")
 
 arg = parser.add_argument
 
-arg('-x', '--bowtie2_index', type=str, help = 'The prefix of the Bowtie2 index of the reference genome') 
+arg('-x', '--bowtie2_index', type=str, help = 'The prefix of the Bowtie2 index of the reference genome')
 arg('-r1', '--rmhost_reads1', type=str, help = 'Query input files are FASTQ .fq/.fastq')
 arg('-r2', '--rmhost_reads2', type=str, help = 'Query input files are FASTQ .fq/.fastq')
 arg('-contig', '--genome_contig_length', type=str, help = 'The file of the reference genomes corresponding to the length of contigs')
-arg('-ID', '--genome_ID', type=str, help = 'The file containing the ID of the reference genome')
-arg('-t', '--threads', metavar = 'N', type=int, default=4, help = 'Number of alignment threads to launch')
-arg('-i', '--identity', metavar = 'N', type=float, default=0.95, help = 'Keep reads larger than N')
-arg('-n', '--reads_num', metavar = 'N', type=int, default=100, help = 'Keep the genome larger than N reads')
-arg('-o', '--output_prefix', type=str, help = 'The prefix name of the output result')
+arg('-t', '--threads', metavar = 'N', type=int, default=4, help = 'Threads for bowtie2')
+arg('-id', '--identity', metavar = 'N', type=float, default=0.95, help = 'Alignments which identity larger than N will be retained')
+arg('-n', '--reads_num', metavar = 'N', type=int, default=100, help = 'Genomes which supported by at least N paired-end reads will be retained')
+arg('-o', '--output_prefix', type=str, help = 'The prefix of the output result')
 
 
 def profile(index,rmhost1,rmhost2,genome_contig_length_file,threads,identity,reads_num,output_prefix):
-    map_file = output_prefix + ".bam"
+    os.mkdir(output_prefix)
+    map_file = os.path.join(output_prefix, output_prefix + ".bam")
     bam_file = open(map_file, "w")
-    cal_file = output_prefix + "_aligned.tsv"
+    cal_file = os.path.join(output_prefix, output_prefix + "_tmp.tsv")
     aligned_file = open(cal_file, "w")
-    subprocess.run("bowtie2 -p %s --very-sensitive --end-to-end -k 2 -x %s -1 %s -2 %s | samtools view -F 2060 -b" %(threads,index,rmhost1,rmhost2),shell=True,encoding="utf-8",stdout=bam_file)
+    subprocess.run("bowtie2 -p %s --sensitive --end-to-end -k 2 -x %s -1 %s -2 %s | samtools view -F 2060 -b" %(threads,index,rmhost1,rmhost2),shell=True,encoding="utf-8",stdout=bam_file)
     bam_file.close()
     aligned_file.write("Read_ID\tContig_ID\tR1_start_loc\tR2_start_loc\tIdentity\n")
     with pysam.AlignmentFile(map_file, "rb") as f:
@@ -54,47 +55,16 @@ def profile(index,rmhost1,rmhost2,genome_contig_length_file,threads,identity,rea
                     r1_alignment_block_length = sum([ int(x) for x in re.findall(r'(\d+)[MDI]', r1_CIGAR) ])
                     r2_alignment_block_length = sum([ int(x) for x in re.findall(r'(\d+)[MDI]', r2_CIGAR) ])
                     identity = (r1_match_bases + r2_match_bases) / (int(r1_alignment_block_length) + int(r2_alignment_block_length))
-                    aligned_file.write("%s\t%s\t%s\t%s\t%s\n" %(r1_ID, r1_contig, r1_start_loc, r2_start_loc, identity))  
+                    aligned_file.write("%s\t%s\t%s\t%s\t%s\n" %(r1_ID, r1_contig, r1_start_loc, r2_start_loc, identity))
     aligned_file.close()
-    filter_file = output_prefix + "_profile.tsv"
-    subprocess.run("Rscript rules/SGB_profile.r %s %s %s %s %s" %(cal_file,filter_file,genome_contig_length_file,identity,reads_num),shell=True)
-
-def summary(genome_ID_file,output_prefix):
-    filter_file = output_prefix + "_profile.tsv"
-    genome_file_SGBID = []
-    filter_file_SGBID = []
-    filter_file_rel_ab = []
-    with open(genome_ID_file, 'rt') as f:
-        for line in f:
-            SGBID = line.strip().split("\t")
-            genome_file_SGBID.append(str(SGBID[0]))
-    with open(filter_file, 'rt') as f:
-        for line in f:
-            massage = line.strip().split("\t")
-            filter_file_SGBID.append(str(massage[0]))
-            filter_file_rel_ab.append(str(massage[5]))
-    summary_file_list = []
-    for i in range(len(genome_file_SGBID)):
-        s = ''
-        if genome_file_SGBID[i] in filter_file_SGBID:
-            j = filter_file_SGBID.index(genome_file_SGBID[i])
-            s = '\t'.join(
-                [genome_file_SGBID[i], filter_file_rel_ab[j]])
-            s += '\n'
-        else:
-            s = '\t'.join(
-                [genome_file_SGBID[i], str(0)])
-            s += '\n'
-        summary_file_list.append(s)
-    summary_file = output_prefix + "_summary.txt"
-    summary_txt = open(summary_file, "w")
-    summary_txt.writelines(summary_file_list)
-    summary_txt.close()
+    filtered_file = os.path.join(output_prefix, output_prefix + "_filtered_align.tsv")
+    profile_file = os.path.join(output_prefix, output_prefix + "_profile.tsv")
+    subprocess.run("Rscript rules/SGB_profile.r %s %s %s %s %s %s" %(cal_file,filtered_file,profile_file,genome_contig_length_file,identity,reads_num),shell=True)
+    os.remove(cal_file)
 
 def main():
     args = parser.parse_args()
     profile(args.bowtie2_index,args.rmhost_reads1,args.rmhost_reads2,args.genome_contig_length,args.threads,args.identity,args.reads_num,args.output_prefix)
-    summary(args.genome_ID,args.output_prefix)
 
 if __name__ == "__main__":
     main()
